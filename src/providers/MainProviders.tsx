20a15e4bc90a285d2df2569fa47198d02f5f6fe4
Shadiq Harwiz <shadiqh.2111@gmail.com>
2025-07-10 14:39:39 +0700

    feat(setup): wrap app with React Query Provider

--- src/providers/MainProviders.tsx
+++ src/providers/MainProviders.tsx
@@ -1,11 +1,15 @@
+import ReactQueryProvider from '@/providers/ReactQueryProvider';
+
 import ReactScan from '@/components/react-scan';
+import { Toaster } from '@/components/ui/toaster';
 
 const MainProviders = ({ children }: React.PropsWithChildren) => {
   return (
-    <>
+    <ReactQueryProvider>
       <ReactScan />
       {children}
-    </>
+      <Toaster />
+    </ReactQueryProvider>
   );
 };
 
